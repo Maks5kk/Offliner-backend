c90869ba0b22c0b3212505fdafa7c7465f2a841a
Maksim <semenik2012@gmail.com>
2025-02-03 21:44:51 +0100

    feature: OFF-4/cors

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import dotenv from "dotenv";
 import { connectDB } from "./lib/db";
 import routes from "./routes/routes";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config();
 
 const PORT = process.env.PORT;
 
 const app = express();
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
